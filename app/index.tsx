720c41f9d5f0e4e48b51d90f2150a3793f5e295f
amaben2020 <60056927+amaben2020@users.noreply.github.com>
2024-11-22 15:34:20 -0600

    project and splash screen setup

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Hello</Text>
-    </SafeAreaView>
+    <View className="flex flex-row border mx-auto my-3">
+      <Text className="text-[50px] font-JakartaExtraBold">Hello</Text>
+
+      <Text className="text-[50px] font-JakartaExtraBold">Hello</Text>
+    </View>
   );
 };
 
